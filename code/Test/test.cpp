eb07ef85108cf236d4abfb1de58d753d429e9b5f
CF\jacky.lee <jacky.lee@chenfull.com.tw>
2026-03-03 09:30:18 +0800

    update

--- code/Test/test.cpp
+++ code/Test/test.cpp
@@ -1,39 +1,11 @@
-#include <stdio.h>
 #include <iostream>
+#include <cstring> // C 字串函式
 
-void bubbleSortDesc(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 1; j < n - i; j++) {
-            if (arr[j - 1] < arr[j]) {
-                std::swap(arr[j - 1], arr[j]);
-            }
-        }
-    }
-}
+int main() {
+    char str1[20] = "Hello";    // 陣列方式
+    char str2[] = "World";      // 自動大小
+    const char* str3 = "C-style"; // 指標方式
 
-
-int main()
-{
-    std::cout << "start..." << std::endl;
-    int counts = 10;
-    int result[counts] = {8, 9, 0, 7, 6, 1, 2, 3, 4, 5};
-
-    bubbleSortDesc(result, counts);
-    /*
-    //泡泡 
-    for (int i = 0; i < counts - 1; i++)
-    {
-        for (int j = 1; j < counts - i; j++)
-        {
-            if (result[j - 1] < result[j])
-            {
-                std::swap(result[j - 1], result[j]);
-            }
-        }
-    }
-    */
-    for (int i = 0; i < counts; i++)
-    {
-        std::cout << i << " :" << result[i] << std::endl;
-    }
+    std::cout << str1 << " " << str2 << " " << str3 << std::endl;
+    return 0;
 }
